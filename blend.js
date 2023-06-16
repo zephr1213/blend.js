72014d7716e6da4ca4b2c8ab49d699420749459f
zephr1213 <56725817+zephr1213@users.noreply.github.com>
2023-06-16 09:25:11 -0500

    Add files via upload

--- blend.js
+++ blend.js
@@ -35,7 +35,20 @@ function formatNumberWithCommas(number) {
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
+// Function to convert a hex color to RGB
+function hextorgb(hex) {
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
 
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
 
 // Function to check if a string is a palindrome
 function isPalindrome(string) {
